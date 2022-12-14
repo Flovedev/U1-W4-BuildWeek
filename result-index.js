0cbc1f03feb8956dc1d8fceeccf567aac3beed0f
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-12-14 09:59:13 +0100

    button to next fixed

--- result-index.js
+++ result-index.js
@@ -53,7 +53,7 @@ showResults();
 
 let buttonToRatingNode = document.getElementById('button_to_rating');
 buttonToRatingNode.addEventListener('click', () =>{
-    console.log(buttonToRatingNode);
+    console.log("works");
     document.getElementById('result_page_container').style.display = "none";
     document.getElementById('feedback-page_content-container').style.display = "inline-block";
 
