0c599b1e7ec3c52b10ff02189ef47dbbf5d7b0ee
Flovedev <flove.dev@gmail.com>
2022-12-15 15:50:52 +0000

    css changes

--- welcome.js
+++ welcome.js
@@ -1,37 +1,37 @@
 let btn = document.getElementById('proceedBtn');
-btn.addEventListener('click', () =>{
-        const checked = document.querySelector('#proceed:checked') !== null;
-        if(checked){
-            document.getElementById('welcomePage').style.display = "none";
-            document.getElementById('questions-part').style.display = "block";
-            document.getElementById('result_page_container').style.display = "none";
-           //time = 20;
-            window.displayQuestion();
+btn.addEventListener('click', () => {
+  const checked = document.querySelector('#proceed:checked') !== null;
+  if (checked) {
+    document.getElementById('welcomePage').style.display = "none";
+    document.getElementById('questions-part').style.display = "block";
+    document.getElementById('result_page_container').style.display = "none";
+    //time = 20;
+    window.displayQuestion();
 
-            var timer = setInterval(function () {
-                if (time > -1) {
-                    console.log(time)
-                    // time = time <= 0 ? 20 : time;
-                    time--;
-                    timerNumber.textContent = time;
-                }
-              
-                if (time === -1) {
-                  // changing the question when time is up
-                  time = 20;
-                  nextQuestion();
-                }
-              
-                // time = 20;
-                // timerNumber.textContent = time;
-              
-                // nextQuestion();
-              
-                if (removeIndex === questionsArray.length) {
-                  document.getElementById("questions-part").style.display = "none";
-                  document.getElementById("result_page_container").style.display = "flex";
-                  clearInterval(timer);
-                }
-              }, 1000);
-        }
+    var timer = setInterval(function () {
+      if (time > -1) {
+        console.log(time)
+        // time = time <= 0 ? 20 : time;
+        time--;
+        timerNumber.textContent = time;
+      }
+
+      if (time === -1) {
+        // changing the question when time is up
+        time = 20;
+        nextQuestion();
+      }
+
+      // time = 20;
+      // timerNumber.textContent = time;
+
+      // nextQuestion();
+
+      if (removeIndex === questionsArray.length) {
+        document.getElementById("questions-part").style.display = "none";
+        document.getElementById("result_page_container").style.display = "flex";
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
 })
