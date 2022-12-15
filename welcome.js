55b1754a68923155ce697c79dd2b7be2760b3ff7
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-12-15 12:46:37 +0100

    display fixed

--- welcome.js
+++ welcome.js
@@ -3,6 +3,6 @@ btn.addEventListener('click', () =>{
         const checked = document.querySelector('#proceed:checked') !== null;
         if(checked){
             document.getElementById('welcomePage').style.display = "none";
-            document.getElementById('questions-part').style.display = "inline-block";
+            document.getElementById('questions-part').style.display = "block";
         }
 })
